f17abffd903f81fb3c6e09164661a95e34954ab7
Luis Vinicius <viniborges@yahoo.com.br>
2026-02-18 22:35:54 -0300

    aula 05 - introducing signals

---
+++ src/app/hello/hello.ts
@@ -0,0 +1,31 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.scss',
+})
+export class Hello {
+  protected title = 'Welcome to Modern Angular!';
+  protected isDisabled = false;
+
+  protected onClick() {
+    console.log("Button clicked");
+    this.isDisabled = !this.isDisabled;
+  }
+
+  protected count = signal(0);
+
+  increateCounter() {
+    this.count.update(value => value + 1);
+  }
+
+  decreaseCounter() {
+    this.count.update(value => value - 1);
+  }
+
+  resetCounter() {
+    this.count.set(0);
+  }
+}
