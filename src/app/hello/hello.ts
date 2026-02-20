f9fd1465b585648fc8a9c1f436fe387e6a1e4f3a
Luis Vinicius <viniborges@yahoo.com.br>
2026-02-20 08:53:43 -0300

    Course 07: Effects (Signals)

--- src/app/hello/hello.ts
+++ src/app/hello/hello.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -18,6 +18,10 @@ export class Hello {
   protected count = signal(0);
 
   protected doubleCount = computed(() => this.count() * 2);
+
+  private readonly countLog = effect(() => {
+    console.log('Count changed: ', this.count())
+  })
 
   protected increateCounter() {
     this.count.update(value => value + 1);
